b27f3a26f6291ca643a329a686541109d74f5896
NapatBourban <napatone123@gmail.com>
2023-02-01 13:03:21 +0700

    finish

--- src/views/News.vue
+++ src/views/News.vue
@@ -146,7 +146,7 @@
         </div>
       </div>
     </div>
-    <div style="margin-top: 15%" class="mx-4 ">
+    <div style="margin-top: 15%" class="mx-4">
       <h3 style="text-align: center; color: aliceblue">
         <div class="shadow-lg p-3 mb-5 bg-black rounded">ข่าวและกิจกรรม</div>
       </h3>
